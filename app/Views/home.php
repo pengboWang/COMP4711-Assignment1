201dd3668f92e7083e3deb064920cb9f51854898
pengboWang <1006736449@qq.com>
2020-11-01 21:06:01 +0800

    added Assignment1

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Lab 06</title>
+	</head>
+	<body>
+		<h1>Royal Never Give Up</h1>
+                <p><a href="/travel">Show the Players</a>.</p>
+		<p><a href="/places">Show the Players raw data</a></p>
+	</body>
+</html>
---
+++ app/Views/oneplace.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Lab 04</title>
+	</head>
+	<body>
+		<h1>List of RNG Players</h1>
+		<p><strong>ID:</strong> {id}</p>
+		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Real name:</strong> {Real name}</p>
+                <p><strong>Age:</strong> {Age}</p>
+                <p><strong>Place:</strong> {Place}</p>
+		<p><strong>Game position:</strong> {Game position}</p>
+                <p><strong>Common heroes:</strong> {Common heroes}</p>
+		<p><img src="/image/{image}"/></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
